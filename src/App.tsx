c3aeb1ced25487acc068d8595134ae4c9c95d501
Oluwatuyi Olufunke <104326012+Olufunmi13@users.noreply.github.com>
2023-09-12 18:51:41 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,24 +27,3 @@ function App() {
 
 export default App;
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAOLpUjPzAng3N1q2RBxMYt9_PbHSHjJnM",
-//   authDomain: "chatapp-30fc9.firebaseapp.com",
-//   projectId: "chatapp-30fc9",
-//   storageBucket: "chatapp-30fc9.appspot.com",
-//   messagingSenderId: "1009579771672",
-//   appId: "1:1009579771672:web:8a53a016ee31086c05006d",
-//   measurementId: "G-CKL6MTFKBK"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
